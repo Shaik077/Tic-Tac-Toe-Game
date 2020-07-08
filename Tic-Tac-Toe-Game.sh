d9abaeb9e39ae147688cec9f20b62c867ccca898
Shaik077 <tshaikvali@gmail.com>
2020-07-08 22:56:24 +0530

    [Shaik]ADDED:Simulation_For_CheckMove

--- Tic-Tac-Toe-Game.sh
+++ Tic-Tac-Toe-Game.sh
@@ -2,9 +2,7 @@
 NUMBEROFROWS=3
 NUMBEROFCOLUMNS=3
 PLACES=0
-row=0
-column=0
-declare -A GameBoard
+declare -A Board
 
 ResetBoard()
 {
@@ -24,11 +22,11 @@ ChecksSymbol()
 {
    if [ $(( RANDOM%2 )) -eq 1 ]
    then
-      PLAYERSYMBOL=O
+      PlayerSymbol=O
    else
-      PLAYERSYMBOL=X
+      PlayerSymbol=X
    fi
-   echo "PlayerSymbol - $PLAYERSYMBOL"
+   echo "PlayerSymbol - $PlayerSymbol"
 }
 ChecksSymbol
 
@@ -57,3 +55,80 @@ TicTacToeBoard()
    done
 }
 TicTacToeBoard
+inputToBoard()
+{
+  local rowIndex=''
+  local columnIndex=''
+
+  for (( row=0; row<$LENGTH; row++))
+  do
+  TicTacToeBoard
+  read PlayerPlace
+
+  if [ $PlayerPlace -gt $LENGTH ]
+  then
+     echo "Invalid"
+     ((row--))
+  else
+  rowIndex=$(( $PlayerPlace / $NUMBEROFROWS ))
+     if [ $(( $PlayerPlace % $NUMBEROFROWS )) -eq 0 ]
+     then
+        rowIndex=$(( $rowIndex - 1 ))
+     fi
+
+  columnIndex=$(( $PlayerPlace %  $NUMBEROFCOLUMNS ))
+     if [ $columnIndex -eq 0 ]
+     then
+        columnIndex=$(( $columnIndex + 2 ))
+     else
+        columnIndex=$(( $columnIndex - 1 ))
+     fi
+
+     if [[ "${Board[$rowIndex,$columnIndex]}" -eq "$PlayerSymbol" ]] || [[ "${Board[$rowIndex,$columnIndex]}" -eq "$computerSymbol" ]]
+     then
+        echo "Invalid move"
+        ((row--))
+     fi
+
+		Board[$rowIndex,$columnIndex]=$PlayerSymbol
+		if [ $(CheckResult) -eq 1  ]
+        then
+           echo "You Won"
+           return 0
+        fi
+  fi
+  done
+	echo "Match Tie"
+}
+
+function CheckResult()
+{
+   if [ $((${Board[0,0]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[0,1]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[0,2]})) -eq $(($PlayerSymbol)) ]
+   then
+      echo 1
+   elif [ $((${Board[1,0]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[1,1]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[1,2]})) -eq $(($PlayerSymbol)) ]
+   then
+      echo 1
+   elif [ $((${Board[2,0]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[2,1]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[2,2]})) -eq $(($PlayerSymbol)) ]
+   then
+      echo 1
+   elif [ $((${Board[0,0]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[1,0]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[2,0]})) -eq $(($PlayerSymbol)) ]
+   then
+      echo 1
+   elif [ $((${Board[0,1]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[1,1]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[2,1]})) -eq $(($PlayerSymbol)) ]
+   then
+      echo 1
+   elif [ $((${Board[0,2]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[1,2]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[2,2]})) -eq $(($PlayerSymbol)) ]
+   then
+      echo 1
+   elif [ $((${Board[0,0]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[1,1]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[2,2]})) -eq $(($PlayerSymbol)) ]
+   then
+      echo 1
+   elif [ $((${Board[2,0]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[1,1]})) -eq $(($PlayerSymbol)) ] && [ $((${Board[0,2]})) -eq $(($PlayerSymbol)) ]
+   then
+      echo 1
+   else
+      echo 0
+   fi
+}  
+
